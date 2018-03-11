3eead6939e716afe80536c81d0c211acee302ee1
Lana-sloth <lana.sloth@gmail.com>
2018-03-11 17:45:53 +0300

    Calculate function added

--- src/app/encounter/encounter.component.ts
+++ src/app/encounter/encounter.component.ts
@@ -12,10 +12,42 @@ import { Component } from '@angular/core';
         <td> <input [(ngModel)]='player.lvl'/> lvl </td>
       </tr>
     </table>
+    <button (click)='calculate()'>Calculate</button>
+    
+    <div *ngIf='encounter.easy > 0'>
+      <div>Easy: {{ encounter.easy }} xp</div>
+      <div>Medium: {{ encounter.medium }} xp</div>
+      <div>Hard: {{ encounter.hard }} xp</div>
+      <div>Deadly: {{ encounter.deadly }} xp</div>
+    </div>
   `
 })
 export class EncounterComponent {
   players = [
     { lvl: 1, amount: 1 }
   ]
+  xpTable = [
+    { lvl: 1, easy: 25, medium: 50, hard: 75, deadly: 100 },
+    { lvl: 2, easy: 50, medium: 100, hard: 150, deadly: 200 }
+  ]
+  encounter = { easy: 0, medium: 0, hard: 0, deadly: 0};
+  calculate(){
+    this.encounter = { easy: 0, medium: 0, hard: 0, deadly: 0};
+    let length = this.players.length
+    for(let i = 0; i < length; i ++) {
+      if(this.players[i].lvl == this.xpTable[i].lvl) {
+        let easy = this.encounter.easy + this.players[i].amount*this.xpTable[i].easy;
+        let medium = this.encounter.medium + this.players[i].amount*this.xpTable[i].medium;
+        let hard = this.encounter.hard + this.players[i].amount*this.xpTable[i].hard;
+        let deadly = this.encounter.deadly + this.players[i].amount*this.xpTable[i].deadly;
+
+        this.encounter = {
+          easy: easy,
+          medium: medium,
+          hard: hard,
+          deadly: deadly
+        }
+      } 
+    }
+  }
 }
